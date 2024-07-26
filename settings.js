85fa54723e0e7db92485dbeb9df87e8cf7775fa8
sobiaxsparle <sobiaxsparle@gmail.com>
2024-07-26 22:41:52 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,26 +4,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923165123719"
-global.ownername = "Umar Rehman"
-global.ytname = "DarkDevil404"
+global.ownernomer = "92251869133"
+global.ownername = "Sobiabutt133"
+global.ytname = "SOBIABUTT133"
 global.socialm = "GitHub: Um4r719"
 global.location = "Chaklala Scheme 3 Rawalpindi,Pakistan"
 
-global.ownernumber = '923165123719'  //creator number
-global.ownername = '𝙐 𝙈 𝘼 𝙍 💛' //owner name
-global.botname = '𝙐𝘿 𝘽𝙐𝙂' //name of the bot
+global.ownernumber = '923251869133'  //creator number
+global.ownername = 'SOBIA 💛' //owner name
+global.botname = 'SOBIA 𝘽𝙐𝙂' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Umar Rehman'
+global.author = 'SOBIA BUTT'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "Dark Devil"
+global.wm = "SOBIA BUTT"
 
 //theme link
-global.link = 'https://wa.me/+923165123719'
+global.link = 'https://wa.me/+92251869133'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
